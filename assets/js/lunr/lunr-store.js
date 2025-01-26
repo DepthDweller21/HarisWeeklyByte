8720069c5e87fd26c8e1339334df3d3a996a0e56
DepthDweller21 <DepthDweller21@users.noreply.github.com>
2025-01-26 16:16:10 +0000

    deploy: c32b25ecd5f730ab4ab15c0894268788b5652452

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Welcome to My Blog!",
+        "excerpt":"currently trying to set this thing up so that people can actually save their eyes from light mode, I intend on posting once a week, with some sort of weekly meme, and/or anime hype, I’ll let the anime hype thing be as is, since they ma include spoilers and I...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/blog/Haris-Weekly-Bits-and-Bytes",
+        "teaser": null
+      },{
+        "title": "ToDo list for new blog",
+        "excerpt":"These are a few features you could look out for in the near future I may work on in my free time, I’ll be updating this list gradually for keep out for updates! Core Features Custom Feeds: Separate feeds for blogs, courses, or other content categories. Custom Theme: A unique...","categories": ["new","blog"],
+        "tags": [],
+        "url": "/blog/ToDo-List-For-Blog",
+        "teaser": null
+      },{
+        "title": "Blog sounds funny",
+        "excerpt":"upon waiting for a very long time, I’m back!! I’m actually really excited to talk about stuff and Ive been brainstorming about this for so long, so excited in fact that…. I forgot what I wanted to talk about… so, heres a ramble about gpus, games, anime, and computers IN...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/blog/Blog-sounds-funny",
+        "teaser": null
+      }]
